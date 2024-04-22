feef7242e5f473cdabf4020551535f7eeca73a50
Arun <arunkushwaha89765@gmail.com>
2024-04-22 23:50:22 +0530

    Update classname for proper selection

--- Firefox/skipper.js
+++ Firefox/skipper.js
@@ -2,15 +2,20 @@ let state = {
  isAdRunning: false,
  muted: false,
 };
-const adCheckingInterval = 1000;
+const adCheckingInterval = 500;
 const videoPlaybackClassName = '.video-stream.html5-main-video';
-const skipAdClassName = '.ytp-ad-preview-image-modern';
-const skipButtonClassName = '.ytp-ad-skip-button-modern.ytp-button';
+const adPreviewIdentifier = '.ytp-preview-ad';
+const adIdentifier = '.ytp-skip-ad';
+const skipButtonIdentifier = '.ytp-skip-ad-button';
 
 const checkForRunningAds = () => {
  //finding skip add elements in dom to know if a ad is running
- const button = document.querySelector(skipAdClassName);
- if (button && !state.isAdRunning) {
+ const button = document.querySelector(adIdentifier);
+ const adPreview = document.querySelector(adPreviewIdentifier);
+ let skipAdButton = document.querySelector(skipButtonIdentifier);
+ if (skipAdButton) {
+  skipAd(skipAdButton);
+ } else if ((button || adPreview) && !state.isAdRunning) {
   console.log('Ad running, encountered first time');
   //ad running for the first time muting the ad
   const videoPlayback = document.querySelector(videoPlaybackClassName);
@@ -26,33 +31,30 @@ const checkForRunningAds = () => {
    videoPlayback.currentTime = videoDuration;
    return;
   }
- } else if (button && state.isAdRunning) {
+ } else if (state.isAdRunning) {
   //ad running checking if we can skip it
   console.log('Ad running');
-  let skipAdButton = document.querySelector(skipButtonClassName);
+  let skipAdButton = document.querySelector(skipButtonIdentifier);
   if (skipAdButton) {
    console.log('We can skip ad');
-   //hurray we can skip the ad
-   skipAdButton.click();
-   //verfying that we skipped the ad
-   skipAdButton = document.querySelector(skipButtonClassName);
-   if (!skipAdButton) {
-    //we skipped the ad
-    console.log('Skipped the ad');
-    const videoPlayback = document.querySelector(videoPlaybackClassName);
-    videoPlayback.muted = false;
-    state.isAdRunning = false;
-    state.muted = false;
-    return;
-   }
+   skipAd(skipAdButton);
   }
- } else {
-  if (state.isAdRunning) {
-   const videoPlayback = document.querySelector(videoPlaybackClassName);
-   if (videoPlayback) videoPlayback.muted = false;
-   state.isAdRunning = false;
-   state.muted = false;
-  }
+ }
+};
+
+const skipAd = (skipAdButton) => {
+ //hurray we can skip the ad
+ skipAdButton.click();
+ //verfying that we skipped the ad
+ skipAdButton = document.querySelector(skipButtonIdentifier);
+ if (!skipAdButton) {
+  //we skipped the ad
+  console.log('Skipped the ad');
+  const videoPlayback = document.querySelector(videoPlaybackClassName);
+  videoPlayback.muted = false;
+  state.isAdRunning = false;
+  state.muted = false;
+  return;
  }
 };
 
